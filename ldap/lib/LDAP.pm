45ad24c7ef63ab758420ef339f3309594fdbfe75
Boyd Duffee <b.duffee@keele.ac.uk>
2015-12-04 11:21:26 +0000

    added routes to LDAP.pm and removed generated test basic.t, now passes tests

--- ldap/lib/LDAP.pm
+++ ldap/lib/LDAP.pm
@@ -8,11 +8,31 @@ sub startup {
   # Documentation browser under "/perldoc"
   $self->plugin('PODRenderer');
 
+  # set secret passphrase
+  $self->secrets(['El Cementerio de los Libros Olvidados']);
+
   # Router
   my $r = $self->routes;
 
   # Normal route to controller
-  $r->get('/')->to('example#welcome');
+  $r->get('/')->to('login#start');
+
+  $r->get('/login')->to('login#login');
+  $r->post('/login')->name('do_login')->to('Login#on_user_login');
+
+  # some protected pages
+  my $authorized = $r->under('/secure')->to('Secure#is_logged_in');
+  $authorized->get('/protected')->to('Secure#protected');
+  $authorized->get('/admin')->to(template => 'secure/admin');
+
+  # this handles logouts
+  $r->route('/logout')->name('do_logout')->to(cb => sub {
+    my $self = shift;
+
+    $self->session(expires => 1);
+
+    $self->redirect_to('/');
+  });
 }
 
 1;
